f1866a1623f4a4ea3d3649def6be6101a3a007a8
Jun Hsieh <jun.github@ijun.org>
2018-10-08 18:12:01 -0700

    add syslog

---
+++ syslog/main.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+ * To let the binary run on alpine, build the main.go with:
+ * CGO_ENABLED=0 go build main.go
+ */
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Hello World\n")
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
